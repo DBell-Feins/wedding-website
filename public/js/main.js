8ccac97f1c65a7de6e1ffb211714168967fbef4e
Dave Bell <bellfazar@gmail.com>
2013-03-10 20:27:16 -0400

    Cleaned up public resources folder

--- public/js/main.js
+++ public/js/main.js
@@ -1,37 +1,13 @@
 /*global $:true, console:true */
-
-
-
 $(function() {
-    $('section.content').height(window.innerHeight);
-    // Set up the slider
-    $('#slides').slidesjs({
-        width: 900,
-        height: 480,
-        play: {
-            active: true,
-            auto: true,
-            interval: 4000,
-            swap: true,
-            effect: 'fade'
-        },
-        pagination: {
-            active: true,
-            effect: 'fade'
-        }
-    });
-    
+    // Slider setup
      $('.flexslider').flexslider({
          slideshow: true,
-         directionNav: true
+         directionNav: true,
+         pauseOnHover: true,
+         slideshowSpeed: 5000
      });
+     
+     // Leaflet setup
+     
 });
-
-function firstCaps(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-}
-function fadeInOut(elOut, elIn) {
-    elOut.fadeOut(400, function() {
-        elIn.fadeIn();
-    });
-}
