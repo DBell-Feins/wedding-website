1cdb2768827e2f6130c453cb373a71b01d84860b
Dave Bell <bellfazar@gmail.com>
2013-03-11 22:38:02 -0400

    Added method to force columns to the same height.

--- public/js/main.js
+++ public/js/main.js
@@ -13,6 +13,8 @@ $(function() {
 
      // Leaflet setup
      if(typeof L !== 'undefined') {
+         $('.location .row-fluid > .span6').eqHeights();
+         
         var cloudmadeUrl = 'http://{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.jpg';
         var subDomains = ['otile1','otile2','otile3','otile4'];
         var cloudmadeAttrib = 'Data, imagery and map information provided by <a href="http://open.mapquest.com" target="_blank">MapQuest</a>, <a href="http://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> and contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/" target="_blank">CC-BY-SA</a>';
