6bf8a4d5228a06111611285ee15dca98a2a4013a
Dave Bell <bellfazar@gmail.com>
2013-04-07 12:22:31 -0400

    Moved js libraries and related css to CDN hosting. Updated references in page controller.

--- application/controllers/page.php
+++ application/controllers/page.php
@@ -5,8 +5,8 @@ class Page_Controller extends Base_Controller {
     public $restful = true;
     /**
      * Static list of pages and their associated attributes and resources
-     * 
-     * At some point it may be worth figuring out a way to have the controller's 
+     *
+     * At some point it may be worth figuring out a way to have the controller's
      * methods define their attributes and resources, but not now.
      * @var array
      */
@@ -19,28 +19,27 @@ class Page_Controller extends Base_Controller {
         'location' => array('slug' => '/location', 'name' => 'Location', 'icon' => 'map-marker'),
         'rsvp' => array('slug' => '/rsvp', 'name' => 'RSVP', 'icon' => 'envelope-alt')
     );
-    
+
     /**
      * Constructor function
      */
     function __construct() {
         // Register scripts
         Asset::container('header')->add('modernizr', 'js/vendor/modernizr-2.6.2-respond-1.1.0.min.js');
-        Asset::container('footer.jquery')->add('jquery', 'js/vendor/jquery-1.9.0.min.js');
-        Asset::container('footer')->add('bootstrap-js', 'js/vendor/bootstrap.min.js');
+        Asset::container('footer')->add('bootstrap-js', 'http://netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/js/bootstrap.min.js');
         Asset::container('footer')->add('flexslider', 'js/vendor/jquery.flexslider-min.js');
         Asset::container('footer')->add('plugins', 'js/plugins.js');
         Asset::container('footer')->add('main', 'js/main.js');
-        
-        
+
+
         // Register CSS
-        Asset::container('header.ie7')->add('font-awesome-ie7', 'css/vendor/font-awesome-ie7.min.css');
-        Asset::container('header')->add('bootstrap-css', 'css/vendor/bootstrap.min.css');
+        Asset::container('header')->add('bootstrap', 'http://netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/css/bootstrap-combined.min.css');
+        Asset::container('header.ie7')->add('font-awesome-ie7', 'http://netdna.bootstrapcdn.com/font-awesome/3.0.2/css/font-awesome-ie7.css');
+        Asset::container('header')->add('font-awesome-css', 'http://netdna.bootstrapcdn.com/font-awesome/3.0.2/css/font-awesome.css');
+
         Asset::container('header')->add('flexslider-css', 'css/vendor/flexslider.css');
-        Asset::container('header')->add('font-awesome-css', 'css/vendor/font-awesome.css');
         Asset::container('header')->add('style', 'css/style.css');
-        Asset::container('header')->add('bootstrap--responsive-css', 'css/vendor/bootstrap-responsive.min.css');
-     
+
         // Call parent constructor to complete Controller registration
         parent::__construct();
     }
@@ -53,7 +52,7 @@ class Page_Controller extends Base_Controller {
     {
         return View::make('page.index', array( 'nav' => Menu::build_menu($this->pages), 'title' => 'We\'re Getting Married!' ));
     }
-    
+
     /**
      * Handles the page@about GET request
      * @return object
@@ -64,7 +63,7 @@ class Page_Controller extends Base_Controller {
         $liz = Person::get_person('liz');
         return View::make('page.about', array( 'nav' => Menu::build_menu($this->pages), 'title' => 'Our Story', 'dave' => $dave, 'liz' => $liz ));
     }
-    
+
     /**
      * Handles the page@photos GET request
      * @return object
@@ -75,7 +74,7 @@ class Page_Controller extends Base_Controller {
         if(count($photos) <= 0)
         {
             return Response::error('404');
-        } 
+        }
         else
         {
             return View::make('page.photos', array( 'nav' => Menu::build_menu($this->pages), 'title' => 'Photos', 'photos' => $photos, 'num' => $num ));
@@ -99,7 +98,7 @@ class Page_Controller extends Base_Controller {
             else
             {
                 return View::make('page.wedding-person', array(
-                    'nav' => Menu::build_menu($this->pages), 
+                    'nav' => Menu::build_menu($this->pages),
                     'person' => $ret_person->to_array(),
                     'title' => $ret_person->name
                 ));
@@ -109,7 +108,7 @@ class Page_Controller extends Base_Controller {
             return View::make('page.wedding', array('nav' => Menu::build_menu($this->pages), 'title' => 'The Wedding Party', 'people' => $people ));
         }
     }
-    
+
     /**
      * Handles the page@registry GET request
      * @return object
@@ -118,7 +117,7 @@ class Page_Controller extends Base_Controller {
     {
         return View::make('page.registry', array( 'nav' => Menu::build_menu($this->pages), 'title' => 'Registry' ));
     }
-    
+
     /**
      * Handles the page@location GET request
      * @return object
@@ -126,13 +125,13 @@ class Page_Controller extends Base_Controller {
     public function get_location()
     {
         // Include leaflet resources
-        Asset::container('header.ie7')->add('leaflet-ie7', 'css/vendor/leaflet.ie.css');
-        Asset::container('header')->add('leaflet-css', 'css/vendor/leaflet.css');
-        Asset::container('footer')->add('leaflet', 'js/vendor/leaflet.js');
-        
+        Asset::container('header.ie7')->add('leaflet-ie7', 'http://cdn.leafletjs.com/leaflet-0.5.1/leaflet.ie.css');
+        Asset::container('header')->add('leaflet-css', 'http://cdn.leafletjs.com/leaflet-0.5.1/leaflet.css');
+        Asset::container('footer')->add('leaflet', 'http://cdn.leafletjs.com/leaflet-0.5.1/leaflet.js');
+
         return View::make('page.location', array( 'nav' => Menu::build_menu($this->pages), 'title' => 'Location' ));
     }
-    
+
     /**
      * Handles the page@rsvp GET request
      * @return object
