d721d7f157b9063274fa03bd73d5249c144e260c
Dave Bell <bellfazar@gmail.com>
2013-04-14 23:04:04 -0400

    Added linked image HTML macro

--- application/start.php
+++ application/start.php
@@ -181,8 +181,16 @@ if ( ! Request::cli() and Config::get('session.driver') !== '')
 |
 */
 HTML::macro('html_link', function($url, $text, $attributes = array(), $https = null)
-    {
-        $url = URL::to($url, $https);
+{
+  $url = URL::to($url, $https);
+  return '<a href="'.$url.'"'.HTML::attributes($attributes).'>'.$text.'</a>';
+});
 
-        return '<a href="'.$url.'"'.HTML::attributes($attributes).'>'.$text.'</a>';
-    });
+HTML::macro('image_link', function($url = '', $img='img/', $alt='', $param = false, $active=true, $ssl=false)
+{
+  $url = $ssl==true ? URL::to_secure($url) : URL::to($url);
+  $img = HTML::image($img,$alt);
+  $link = $active==true ? HTML::link($url, '#', $param) : $img;
+  $link = str_replace('#',$img,$link);
+  return $link;
+});
